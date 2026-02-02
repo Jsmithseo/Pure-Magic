2c64b03a5e5acfd161a70ca52057c164308656f6
Jeremy smith <jeremysmithseo@gmail.com>
2026-02-02 06:41:04 -0800

    wip

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/smp",
+        permanent: false, // set to true if you want a permanent 308 redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
